2748c9ab229aa0ae3c656265a51df730bca628c1
Saran-k-ece <saran.k2022ece@sece.ac.in>
2025-10-22 08:59:40 +0530

    Db changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://ksaran0006_db_user:fooddel@cluster0.uj9pwnz.mongodb.net/FoodDelivery').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://ksaran0006_db_user:food@cluster0.ragbroc.mongodb.net/FoodDelivery').then(()=>console.log("DB Connected"));
    
 }
 
